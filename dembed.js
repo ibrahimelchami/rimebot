79ba86a0fdb262880509b2c935baf45ddd7744cb
Ibrahim El Chami <ibelchami@gmail.com>
2019-12-10 02:34:00 -0800

    Removing debug line and a little text clean up.

--- dembed.js
+++ dembed.js
@@ -4,7 +4,6 @@ module.exports = {
     getInfoCard: function(color, name, type, sprite_shiny, sprite_reg) {
         var pokeEmbed = new Discord.RichEmbed()
         pokeEmbed.setColor(color)
-        console.log("hi")
         pokeEmbed.setTitle(name)
         pokeEmbed.addField('Type', type)
         pokeEmbed.setThumbnail(sprite_shiny)
--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 const {prefix, token} = require('./config.json');
 const client = new Discord.Client();
 
+const dEmbed = require('./dembed');
+
 function titleCase(str) {
     str = str[0].toUpperCase() + str.substr(1);
     return str;
 }
-
-// const fetch = require("node-fetch");
 
 const vowels = 'aeiouAEIOU';
 
@@ -15,20 +15,11 @@ client.once('ready', () => {
 console.log('Ready!')
 })
 
-// function probability(n) {
-//   return Math.random() <= n;
-// }
-
 client.on('message', message => {
     if(message.content.startsWith(`${prefix}`) && (!message.author.bot)) {
-        // console.log(message.content);
-
         //kickFunction
         if (message.member.hasPermission(['KICK_MEMBERS', 'BAN_MEMBERS'])) {
-
             if(message.content.startsWith(`${prefix}kick`)) {
-                //message.channel.send("No!")
-
                 let member = message.mentions.members.first();
                 if(member != undefined) {
                     member.kick().then((member) => {
@@ -41,29 +32,25 @@ client.on('message', message => {
         //PokedexTypeSerch
         if(message.content.startsWith(`${prefix}dex`)) {
                 let search = message.content.toLowerCase().replace('.', '');
-                console.log(search)
                 if(search.includes("mr mime") || search.includes("mime jr") || search.includes("tapu koko")) {
                     search = search.split(" ")[1] + '-' + search.split(" ")[2];
                 } else {
                     search = search.split(" ")[1];
                 }
-                console.log(search)
                 let pokemon = search;
                 let Pokedex = require("pokedex-promise-v2");
                 let Poke = {};
             Poke["pokedex-promise-v2"] = new Pokedex()
             pokemon = Poke["pokedex-promise-v2"].getPokemonByName(pokemon) // with Promise
+
             .then(function (response) {
-                // console.log(response);
                 let pokedata = response;
                 //id
                 let pokeid = String(pokedata.id);
-                console.log(pokeid.length);
                 if(pokeid.length < 3) {
                     if(pokeid.length > 1) {pokeid = '0' + pokeid}
                     else(pokeid = '00' + pokeid)
                 }
-                console.log(pokeid.length);
                 //name&grammar
                 let pokename = pokedata.forms[0].name;
                 pokename = titleCase(pokename);
@@ -74,43 +61,41 @@ client.on('message', message => {
                 } else {
                     type = titleCase(pokedata.types[0].type.name);
                 }
-                let pokeEmbed = new Discord.RichEmbed()
-                pokeEmbed.setColor('#FF0000')
-                pokeEmbed.setTitle(pokename)
-                pokeEmbed.addField('Type', type)
-                pokeEmbed.setThumbnail('https://www.serebii.net/Shiny/SWSH/' + pokeid + '.png')
-                pokeEmbed.setImage('https://www.serebii.net/swordshield/pokemon/' + pokeid + '.png')
-
-                // let grmr = ' is a '
-                // if(vowels.indexOf(pokedata.types[0].type.name[0]) == 1) {
-                //     grmr = ' is an '
-                // }
-                // if(pokedata.types.length == 2) {
-                //     type = pokename + grmr + pokedata.types[1].type.name + ' & ' + pokedata.types[0].type.name + ' type pokemon! Rime!';
-                // } else {
-                //     type = pokename + grmr + pokedata.types[0].type.name + ' type pokemon! Rime!';
-                // }
-                // message.channel.send(type);
+                let sprite_shiny = 'https://www.serebii.net/Shiny/SWSH/' + pokeid + '.png'
+                let sprite_reg = 'https://www.serebii.net/swordshield/pokemon/' + pokeid + '.png'
+                let color = "#FF0000";
+                let pokeEmbed = dEmbed.getInfoCard(color, pokename, type, sprite_shiny, sprite_reg);
                 message.channel.send(pokeEmbed);
             })
 
             .catch(function (error) {
-                 message.channel.send("That's not a Pokémon!");
+                message.channel.send("That's not a Pokémon!");
             });
         }
     }
 
 })
 
+client.login(token);
+
+
+
+// let grmr = ' is a '
+// if(vowels.indexOf(pokedata.types[0].type.name[0]) == 1) {
+//     grmr = ' is an '
+// }
+// if(pokedata.types.length == 2) {
+//     type = pokename + grmr + pokedata.types[1].type.name + ' & ' + pokedata.types[0].type.name + ' type pokemon! Rime!';
+// } else {
+//     type = pokename + grmr + pokedata.types[0].type.name + ' type pokemon! Rime!';
+// }
+// message.channel.send(type);
+
 // let str1 = 'ello';
 // var vowel_list = 'aeiouAEIOU'
 // console.log(vowel_list.indexOf(str1[0]);
 
 // types: [ { slot: 1, type: [Object] } ]
-
-
-
-client.login(token);
 
 // let Pokedex = require('pokedex-promise-v2');
 // let Poke = {};
@@ -124,6 +109,7 @@ client.login(token);
 //     console.log('There was an ERROR: ', error);
 // });
 
+// const fetch = require("node-fetch");
 // const apiData = {
 //     url: 'https://pokeapi.co/api/v2/',
 //     type: 'pokemon-species',
