eed95043d1f9a5af581528219017852a05219281
Ibrahim El Chami <ibelchami@gmail.com>
2019-12-10 00:53:37 -0800

    adding all files

---
+++ test.js
@@ -0,0 +1,17 @@
+function titleCase(str) {
+    str = str[0].toUpperCase() + str.substr(1);
+    return str;
+}
+
+console.log(titleCase("hello"))
+
+function showMessage() {
+  let message = "Hello, I'm JavaScript!"; // local variable
+
+  console.log( message );
+}
+showMessage()
+
+
+var hi = "hello my name is"
+console.log('!dex Mr. Mime'.includes('Mr. Mime'))
